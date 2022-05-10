c80953c6ef18b4b11a453d70b4723d07ff106bd3
mikkimike <mihail.s@ontid.com>
2022-05-10 13:47:05 +0300

    related

--- src/Services/RelationService.php
+++ src/Services/RelationService.php
@@ -2,16 +2,35 @@
 
 namespace Mikkimike\Exchange1C\Services;
 
+use Mikkimike\Exchange1C\Config;
 use Mikkimike\Exchange1C\Events\ImportLog;
+use Mikkimike\Exchange1C\Interfaces\EventDispatcherInterface;
+use Mikkimike\Exchange1C\Interfaces\ModelBuilderInterface;
 use Mikkimike\Exchange1C\PayloadTypes\BatchStart;
 use Mikkimike\Exchange1C\PayloadTypes\RelationProducts;
 
 class RelationService
 {
+    /**
+     * @var Request
+     */
+    private $request;
+    /**
+     * @var Config
+     */
+    private $config;
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $dispatcher;
+    /**
+     * @var ModelBuilderInterface
+     */
+    private $modelBuilder;
+
     public function import()
     {
         $filename = basename($this->request->get('filename'));
-        $this->_ids = [];
         $category = false;
         if ($this->request->has('category')) {
             $category = $this->request->get('category');
